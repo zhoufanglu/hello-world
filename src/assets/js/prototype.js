758eb4b26199fb6a312311b821bd031c73740931
zhoufanglu <735417031@qq.com>
2019-05-10 18:06:38 +0800

    update element message

--- src/assets/js/prototype.js
+++ src/assets/js/prototype.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 //接口配置
 import api from '@/request/api'
 
+/**
+ * element-ui
+ * 按需引入
+ */
+import '@/assets/scss/element/element-variables.scss'
+import { Button, Form ,Radio, Input, Select ,Checkbox, FormItem, Message} from 'element-ui';
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(Radio)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Checkbox)
+Vue.use(FormItem)
+//Vue.use(Message)
+Vue.component(Message.name, Message)
+
 const prototype = ()=>{
   Vue.prototype.$api = api
+  Vue.prototype.$message = Message
 }
 export default prototype
 
--- src/main.js
+++ src/main.js
@@ -14,24 +14,7 @@ Vue.use(prototype)
 /**
  * 插件引入文件
  */
-import plugins from '@/assets/js/plugins'
-
-/**
- * element-ui
- * 按需引入
- */
-import '@/assets/scss/element/element-variables.scss'
-import { Button, Form ,Radio, Input, Select ,Checkbox, FormItem, Message} from 'element-ui';
-Vue.use(Button)
-Vue.use(Form)
-Vue.use(Radio)
-Vue.use(Input)
-Vue.use(Select)
-Vue.use(Checkbox)
-Vue.use(FormItem)
-Vue.use(Message)
-
-
+import '@/assets/js/plugins'
 
 Vue.config.productionTip = false
 
