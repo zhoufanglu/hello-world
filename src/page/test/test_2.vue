a2acec138659f0bb184258e032428cfd9cac0b10
卢方舟 <lufangzhou@adinnet.cn>
2020-01-14 19:24:50 +0800

    study by mu yi yang

--- src/page/test/test_2.vue
+++ src/page/test/test_2.vue
@@ -1,82 +1,84 @@
 <template>
     <div>
-        <h2>test_2</h2>
+        <h2>test_2...</h2>
         <ul>
             <li v-for="(i,index) in list" :key="index">{{ i.name | filtersName }}</li>
+            ----
+            <li v-for="(i,index2) in list" :key="index2+i.name">{{i.name}}</li>
         </ul>
+        <button @click="changeAge">点我改变多层obj数据</button>
+        <span>age:{{obj.father.age}}</span>
     </div>
 </template>
 
 <script>
-  import {formatDateTime} from "../../assets/js/utils/validate";
-  export default {
-    name: "test_2",
-    data() {
-      return {
-        list:[
-          {name:'lfz'},
-          {name:'sxy'},
-          {name:'ljd'},
-          {name:'xjy'},
-        ]
-      }
-    },
-    methods: {
+    import {formatDateTime} from "../../assets/js/utils/validate";
 
-    },
-    created(){
-      let data = { foo: 'foo' }
-      let p = new Proxy(data, {
-        get(target, key, receiver) {
-          return target[key]
+    export default {
+        name: "test_2",
+        data() {
+            return {
+                list: [
+                    {name: 'lfz'},
+                    {name: 'sxy'},
+                    {name: 'ljd'},
+                    {name: 'xjy'},
+                ],
+                obj:{
+                    father:{
+                        name:'lfz',
+                        age: 16
+                    }
+                }
+            }
         },
-        set(target, key, value, receiver) {
-          console.log('set value',value)
-          target[key] = value // ?
-          return true
+        methods: {
+            changeAge(){
+                this.obj.father.age = 19
+            }
+        },
+        created() {
+            let data = {foo: 'foo'}
+            let p = new Proxy(data, {
+                get(target, key, receiver) {
+                    return target[key]
+                },
+                set(target, key, value, receiver) {
+                    console.log('set value', value)
+                    target[key] = value // ?
+                    return true
+                }
+            })
+
+            console.log(40, p.foo = 'xx')
+            console.log(41, p.foo = 'yy')
+
+
+        },
+        mounted() {
+
+
+        },
+        computed: {
+            computeVal() {
+                return (val) => {
+                    console.log('computed')
+                    return val.toUpperCase()
+                }
+            }
+        },
+        filters: {
+            filtersName(name) {
+                return name.toUpperCase()
+            }
+        },
+        watch: {
+            list() {
+                //console.log(48,this)
+            }
         }
-      })
 
-      console.log(40,p.foo='xx')
-      console.log(41,p.foo='yy')
-
-
-    },
-    mounted() {
-
-      this.list = []
-
-      let handler = {
-        get: function(target, name){
-          console.log(35,target)
-          return name in target ? target[name] : 37;
-        }
-      }
-      let p = new Proxy({}, handler);
-
-
-
-    },
-    computed:{
-      computeVal(){
-        return (val)=>{
-          console.log('computed')
-          return val.toUpperCase()
-        }
-      }
-    },
-    filters:{
-      filtersName(name){
-        return name.toUpperCase()
-      }
-    },
-    watch:{
-      list(){
-        //console.log(48,this)
-      }
     }
-
-  }
 </script>
 <div>sds</div>
 <style lang="scss" scoped>
